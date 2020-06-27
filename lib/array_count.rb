648eab62d3c0c2bb4f4c2261658fe1e67d9f27e7
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-27 17:59:20 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,11 @@
 def count_strings(array)
   array.count do |element| 
-  element.class == String
+    element.class == String
+  end
 end
 
 def count_empty_strings(array)
-  array.count { |element| element == "" }
+  array.count do |element| 
+    element == "" 
+  end
 end
